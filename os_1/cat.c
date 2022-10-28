3abab4dc05c7a66ba5ece26949a7ddbdeb4a690d
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2022-10-28 14:08:04 +0530

    Update cat.c

--- os_1/cat.c
+++ os_1/cat.c
@@ -1,19 +1,64 @@
-#include<stdio.h>
-#include<string.h>
+// done
+#include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <unistd.h>
-int main(int argc, char **argv){
-    int fp;
-    char ch[99]; 
-    int op;
-    fp=open(argv[0], O_RDONLY);
-    while(op=read(fp,ch,99)){    
-        write(1,ch,op);
-    }
-    printf("\n");
-    close(fp);
-    return 0;
+#include<string.h>
+
+void main(int argc, char *c[]){
+	printf(".");
+	FILE *file_p;
+	if(argc == 1){
+		printf("ERROR: :Enter file name\n");
+	}
+	if(strcmp(c[1],"-n")==0){
+		file_p =fopen(c[2],"r");
+		if( file_p == NULL){
+			perror("Error: ");
+		}
+		else{
+			char * buffer=NULL;
+			size_t l=0;
+			ssize_t read;
+			int count=1;
+			while ((read= getline(&buffer,&l,file_p)) != -1){
+				printf("%d %s",count++,buffer );
+			}
+			fclose(file_p);
+		}
+	}
+	else if(strcmp(c[1],"-E")==0){
+		file_p =fopen(c[2],"r");
+		if( file_p == NULL){
+			perror("Error: ");
+		}
+		else{
+			char * buffer=NULL;
+			size_t l=0;
+			ssize_t read;
+			
+			while ((read= getline(&buffer,&l,file_p)) != -1){
+				strtok(buffer, "\n");
+				printf("%s$ \n",buffer );
+			}
+			fclose(file_p);
+		}
+	}
+	else{
+		file_p =fopen(c[1],"r");
+		if( file_p == NULL){
+			perror("Error: ");
+		}
+		else{
+			char * buffer=NULL;
+			size_t l=0;
+			ssize_t read;
+			
+			while ((read= getline(&buffer,&l,file_p)) != -1){
+				printf("%s ",buffer );
+			}
+			fclose(file_p);
+		}
+	}
+	
+	
 }
+
