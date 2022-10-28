77376001b9692397ba8f14c0ce4d0bd620846c0d
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2022-10-28 14:08:46 +0530

    Update ls.c

--- os_1/ls.c
+++ os_1/ls.c
@@ -1,29 +1,53 @@
- #include<stdio.h>
-#include<dirent.h>
-#include<string.h>
-#include<stdlib.h>
-void recursive(const char *name, int indent);
-
-int main(int argc,char **argv)
- {
-
-   struct dirent **name;
-   int n;
-
-   if (argc == 1)
-    {
-     n=scandir(".",&name,NULL,alphasort);
-      while (n--)
-       {
-         printf("%s\n",name[n]->d_name);
-         free(name[n]);
-       }
-      free(name);
-   }
+//done
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 
-    
-   exit(EXIT_SUCCESS);
-   }
 
+void main(int argc, char *c[]){
+
+	
+	DIR *folders;
+	struct dirent *file_names;
+	folders = opendir(".");
+	
+
+	if(argc==1){
+		printf("\n");
+		if(folders!= NULL){
+			while((file_names= readdir(folders)) != NULL){
+				if(!(strcmp(file_names->d_name,".")==0 || strcmp(file_names->d_name,"..")==0))
+					printf("%s \t",file_names->d_name );
+			}
+			printf("\n");
+		}
+		closedir(folders);
+	}
+
+	if(strcmp(c[1],"-m")==0){
+		printf("\n");
+		if(folders!= NULL){
+			while((file_names= readdir(folders)) != NULL){
+				if(!(strcmp(file_names->d_name,".")==0 || strcmp(file_names->d_name,"..")==0))
+					printf("%s,\t",file_names->d_name );
+			}
+			printf("\n");
+		}
+		closedir(folders);
+	}
+	else if(strcmp(c[1],"-1")==0){
+		if(folders!= NULL){
+			while((file_names= readdir(folders)) != NULL){
+				if(!(strcmp(file_names->d_name,".")==0 || strcmp(file_names->d_name,"..")==0))
+					printf("%s\n",file_names->d_name );
+			}
+		}
+		closedir(folders);
+	}
+	
+}
 
