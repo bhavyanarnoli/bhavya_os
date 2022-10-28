efcd98e1647965f12872683f317e40301e41fa0f
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2022-10-28 14:07:02 +0530

    Update mkdir.c

--- os_1/mkdir.c
+++ os_1/mkdir.c
@@ -1,15 +1,100 @@
+// done
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include<string.h>
+#include<stdlib.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-int main(int argc, char**argv){
-    int ret = 0;
-    ret = mkdir(argv[0], 0755);
-    if (ret == 0)
-        printf("Directory created successfully %s\n ",argv[0]);
-    else
-        printf("Unable to create directory %s\n", argv[0]);
-    return 0;
+void main(int argc, char *c[]){
+
+	if(argc==1){
+		printf("Enter a name\n");
+	}
+
+	if(strcmp(c[1],"-v")==0){
+		DIR* dir =opendir(c[2]);
+		if(dir){
+			printf("Directory already exists\n");
+			closedir(dir);
+		}
+		else{
+			mkdir((const char *)c[2], S_IRWXU);
+			printf("Directory has been created \n" );
+		}
+	}
+	else if(strcmp(c[1],"-m")==0){
+		int mode =atoi(c[2]);
+
+		DIR* dir =opendir(c[3]);
+		if(dir){
+			printf("Directory already exists\n");
+			closedir(dir);
+		}
+		else{
+			mkdir((const char *)c[3], mode);
+			printf("Directory has been created \n" );
+		}
+	}
+	else{
+		DIR* dir =opendir(c[1]);
+		if(dir){
+			printf("Directory already exists\n");
+			closedir(dir);
+		}
+		else{
+			mkdir((const char *)c[1], S_IRWXU);
+		}
+	}
 }
+
+// done
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+void main(int argc, char *c[]){
+
+	if(argc==1){
+		printf("Enter a name\n");
+	}
+
+	if(strcmp(c[1],"-v")==0){
+		DIR* dir =opendir(c[2]);
+		if(dir){
+			printf("Directory already exists\n");
+			closedir(dir);
+		}
+		else{
+			mkdir((const char *)c[2], S_IRWXU);
+			printf("Directory has been created \n" );
+		}
+	}
+	else if(strcmp(c[1],"-m")==0){
+		int mode =atoi(c[2]);
+
+		DIR* dir =opendir(c[3]);
+		if(dir){
+			printf("Directory already exists\n");
+			closedir(dir);
+		}
+		else{
+			mkdir((const char *)c[3], mode);
+			printf("Directory has been created \n" );
+		}
+	}
+	else{
+		DIR* dir =opendir(c[1]);
+		if(dir){
+			printf("Directory already exists\n");
+			closedir(dir);
+		}
+		else{
+			mkdir((const char *)c[1], S_IRWXU);
+		}
+	}
+}
+
