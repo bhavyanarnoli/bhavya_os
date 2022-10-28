5476656ab74650604dd76cec438d1b38bb393395
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2022-10-28 14:06:14 +0530

    Update shell.c

--- os_1/shell.c
+++ os_1/shell.c
@@ -6,6 +6,19 @@
 #include<sys/wait.h>
 #include<readline/readline.h>
 #include<readline/history.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <libgen.h> 
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <dirent.h>
 
 #define MAXCOM 1000 
 #define MAXLIST 100 
@@ -27,8 +40,7 @@ void init_shell()
 }
 
 int takeInput(char* str)
-{
-	char* buf;
+{	char* buf;
 
 	buf = readline("\n>>> ");
 	if (strlen(buf) != 0) {
@@ -57,31 +69,29 @@ void execArgs(char** parsed){
 	} 
 	else if (pid == 0) {
         if (!strcmp(parsed[0],"date")){
-            execl("./date",NULL);
-        }
-		else if(!strcmp(parsed[0],"ls") || !(strcmp(parsed[0],"ls -R"))){
-			execl("./ls",NULL);
-		}
-		else if(!strcmp(parsed[0],"ls -al")){
-			execl("./ls1",NULL);
-		}
-		else if(!strcmp(parsed[0],"ls | wc")){
-			execl("./lswc",NULL);
-		}
+           	execl("./date",parsed[0],parsed[1],NULL);}
+			
+		else if(!strcmp(parsed[0],"ls")){
+			execl("./ls",parsed[0],parsed[1],NULL);}
+		
 		else if(!strcmp(parsed[0],"cat")){
-			execl("./cat",parsed[1],parsed[1],NULL);
-		}
+			execl("./cat",parsed[0],parsed[1],parsed[2],NULL);}
+
+		
 		else if(!strcmp(parsed[0],"rm")){
-			execl("./rm",parsed[1],parsed[1],NULL);
-		}
+			
+			execl("./rm",parsed[0],parsed[1],parsed[2],NULL);}
+			
+	
 		else if(!strcmp(parsed[0],"mkdir")){
-			execl("./mkdir",parsed[1],parsed[1],NULL);
+			execl("./mkdir", parsed[0],parsed[1],parsed[2],parsed[3], NULL);
+			
 		}
         else{
             printf("Command not recognized!!");
-        }
-		exit(0);
-	} 
+			exit(0);
+        }}
+	 
 	else {
 		wait(NULL);
 		return;}
@@ -110,8 +120,16 @@ int ownCmdHandler(char** parsed)
 		printf("\nGoodbye\n");
 		exit(0);
 	case 2:
-		chdir(parsed[1]);
-		return 1;
+		if(parsed[1]==NULL){
+		chdir(parsed[1]);}
+		else{
+			int check=chdir(parsed[1]);
+			char buff[1000];
+			char*present_dir=getcwd(buff,sizeof(buff));
+			if(check==-1){
+				printf("Invalid path\n");
+			}
+		}
 
 	case 3:
 		while (*++parsed) {
@@ -121,7 +139,7 @@ int ownCmdHandler(char** parsed)
     	printf("\n");
 		return 1;
 	case 4: 
-		execl ("/bin/pwd", "pwd", NULL);
+		execl("/bin/pwd", "pwd", NULL);
 		return 1;
 	default:
         execArgs(parsed);
