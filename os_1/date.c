349948ecda7dcfd201cc6efee8a376d01b0e8c04
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2022-10-28 14:07:32 +0530

    Update date.c

--- os_1/date.c
+++ os_1/date.c
@@ -1,13 +1,34 @@
-#include <assert.h>
-#include <stdio.h>
-#include <time.h>
+// done
 
-int main(void) {
-    time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-    char s[64];
-    size_t ret = strftime(s, sizeof(s), "%c", tm);
-    assert(ret);
-    printf("%s\n", s);
-    return 0;
+#include <time.h>
+#include<string.h>
+#include<stdio.h>
+void main(int argc, char *c[]){
+	time_t Time;
+	struct tm *tm;
+	time(&Time);
+	tm=localtime(&Time);
+	char buffer[100];
+
+	if(argc==1){
+
+		time_t Time;
+		struct tm *tm;
+		time(&Time);
+		tm=localtime(&Time);
+		char buffer[100];
+		strftime(buffer,100,"%a %b %d %X %Z %Y ", tm);
+		printf("%s\n", buffer);
+	}
+
+	if(strcmp(c[1],"-I")==0){
+		strftime(buffer,100,"%Y-%m-%d ", tm);
+		printf("%s\n", buffer);
+	}
+	else if (strcmp(c[1],"-R")==0){
+		strftime(buffer,100,"%a, %d %b %Y %X %Z ", tm);
+		printf("%s +0530\n", buffer);
+	}
+
 }
+
