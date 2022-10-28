5e52a4446ce2aa19792cd4996fa9b1cbd0191b87
Bhavya Narnoli <108413315+bhavyanarnoli@users.noreply.github.com>
2022-10-28 14:06:39 +0530

    Update rm.c

--- os_1/rm.c
+++ os_1/rm.c
@@ -1,18 +1,77 @@
+//done
+
 #include <stdio.h>
-#include <stdlib.h>
+#include<string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <dirent.h>
 
-#include<stdio.h>
+void main(int argc, char *c[]){
+	if(argc==1){
+		printf("Enter a name\n");
+	}
+	if(strcmp(c[1],"-v")==0){
+		DIR *dir =opendir(c[2]);
+		if(dir){
+			printf("It is a Directory\n");
+			closedir(dir);
+		}
+		else{
+			int result=remove(c[2]);
+			if(result == 0){
+				printf("File deleted successfully \n" );
+			}
+			else{
+				printf("Error: unable to delete the file");
+			}
+		}
+	}
+	else if(strcmp(c[1],"-i")==0){
+		printf("Do you want to delete this file\n");
+		char ans[10];
+		gets(ans);
+		char *temp;
+		temp=strtok(ans," ");
+		printf("%stemp\n",temp );
+		if (strcmp(temp,"no")==0 ||strcmp(temp,"No")==0 )
+		{
+			printf("++++\n");
+		}
+		else{
+			printf("yoyo\n");
+			DIR *dir =opendir(c[2]);
+			if(dir){
+				printf("It is a Directory\n");
+				closedir(dir);
+			}
+			else{
+				int result=remove(c[2]);
+				if(result == 0){
+					printf("File deleted successfully \n" );
+				}
+				else{
+					printf("Error: unable to delete the file");
 
-void main(int argc, char* argv[]){
-    if(argc!=2 || argv[0]=="--help"){
-        printf("\nusage: rm FileTodelete\n");}
-    int status;
-    status=remove(argv[1]);
-    if(status==0) {
-        printf("successfull\n");
-    }
-    else{
-        printf("Unsuccessfull\n");
-    }
+				}
+			}
+		}
+	}
+	else{
+		DIR *dir =opendir(c[1]);
+		if(dir){
+			printf("It is a Directory\n");
+			closedir(dir);
+		}
+		else{
+			int result = remove(c[1]);
+			if(result == 0){
+				printf("File deleted successfully \n" );
+			}
+			else{
+				printf("Error: unable to delete the file");
+
+			}
+		}
+	}
 }
+
